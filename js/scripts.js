caf6358f0d0d9b742cad63438d11ee25373eda4e
andouros <149954032+andouros@users.noreply.github.com>
2024-04-17 10:00:16 +0200

    Update scripts.js
    stop modal on mobile

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
   const clickableImages = document.querySelectorAll('.clickable-image');
-  const modalImage = document.getElementById('enlargedImage');
 
   clickableImages.forEach(image => {
-    image.addEventListener('click', function() {
+    image.addEventListener('click', function(event) {
       const enlargedImageSrc = this.getAttribute('data-src');
       
-
       const modalId = this.getAttribute('data-modal-target');
       const modalTarget = document.getElementById(modalId);
       
       if (window.innerWidth <= 768) {
-        return;
+        event.preventDefault(); // Prevent default click action
+        event.stopPropagation(); // Stop event propagation
+        return; // Exit the function without opening modal
       }
 
-      modalImage.src = enlargedImageSrc;
       modalTarget.querySelector('img').src = enlargedImageSrc;
     });
   });
