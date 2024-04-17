73373f893b06d5c73492eacd0139071276ce4833
andouros <149954032+andouros@users.noreply.github.com>
2024-04-17 10:52:10 +0200

    Update scripts.js
    remove modal from touch screens

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const clickableImages = document.querySelectorAll('.clickable-image');
+  if (document.querySelector('html').classList.contains('no-touch')) {
+    const clickableImages = document.querySelectorAll('.clickable-image');
 
-  clickableImages.forEach(image => {
-    image.addEventListener('click', function() {
-      const enlargedImageSrc = this.getAttribute('data-src');
-      const enlargedImageElement = document.getElementById('enlargedImage');
+    clickableImages.forEach(image => {
+      image.addEventListener('click', function() {
+        const enlargedImageSrc = this.getAttribute('data-src');
+        const enlargedImageElement = document.getElementById('enlargedImage');
 
-      enlargedImageElement.src = enlargedImageSrc;
+        enlargedImageElement.src = enlargedImageSrc;
+      });
     });
-  });
+  }
 });
+
