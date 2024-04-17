4017d3d647554d504cc6a18a10391391fb092886
andouros <149954032+andouros@users.noreply.github.com>
2024-04-17 10:03:52 +0200

    Update scripts.js
    back to normal

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,11 @@ document.addEventListener('DOMContentLoaded', function() {
   const clickableImages = document.querySelectorAll('.clickable-image');
 
   clickableImages.forEach(image => {
-    image.addEventListener('click', function(event) {
+    image.addEventListener('click', function() {
       const enlargedImageSrc = this.getAttribute('data-src');
-      
-      const modalId = this.getAttribute('data-modal-target');
-      const modalTarget = document.getElementById(modalId);
-      
-      if (window.innerWidth <= 768) {
-        event.preventDefault(); // Prevent default click action
-        event.stopPropagation(); // Stop event propagation
-        return; // Exit the function without opening modal
-      }
+      const enlargedImageElement = document.getElementById('enlargedImage');
 
-      modalTarget.querySelector('img').src = enlargedImageSrc;
+      enlargedImageElement.src = enlargedImageSrc;
     });
   });
 });
