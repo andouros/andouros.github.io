5e7e2859dab24581c1e92865bf7ecb0c9e9aae3a
andouros <149954032+andouros@users.noreply.github.com>
2024-04-17 09:54:40 +0200

    Update scripts.js
    add js

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
   const clickableImages = document.querySelectorAll('.clickable-image');
+  const modalImage = document.getElementById('enlargedImage');
 
   clickableImages.forEach(image => {
     image.addEventListener('click', function() {
       const enlargedImageSrc = this.getAttribute('data-src');
-      const enlargedImageElement = document.getElementById('enlargedImage');
+      
 
-      enlargedImageElement.src = enlargedImageSrc;
+      const modalId = this.getAttribute('data-modal-target');
+      const modalTarget = document.getElementById(modalId);
+      
+      if (window.innerWidth <= 768) {
+        return;
+      }
+
+      modalImage.src = enlargedImageSrc;
+      modalTarget.querySelector('img').src = enlargedImageSrc;
     });
   });
 });
